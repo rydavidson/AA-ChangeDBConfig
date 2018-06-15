5fbdc4c6bb37592a8058159a7440cb694be2bb00
Ryan Davidson <rydavidson@accela.com>
2018-06-15 10:27:09 -0600

    Added logging
    Added Logger class with async logging functions

--- AA-ChangeDBConfig/Business/CommonUtils.cs
+++ AA-ChangeDBConfig/Business/CommonUtils.cs
@@ -1,18 +1,90 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AA_ChangeDBConfig.Business
 {
     static class CommonUtils
     {
-        public static List<string> getInstances()
+        public static Logger logger = new Logger("CommonUtils.log");
+        public static List<string> GetInstancesForVersion(string versionToCheck)
         {
             List<string> instances = new List<string>();
+            try
+            {
+                RegistryKey accelaBaseKey = GetAccelaBaseKey();
+                List<string> versions = GetAAVersions();
+                foreach (string version in versions)
+                {
+                    if (version.Trim() == versionToCheck.Trim())
+                    {
+                        foreach (string instance in GetAAInstancesByVersion(version))
+                        {
+                            instances.Add(instance);
+                        }
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException uaex)
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
 
             return instances;
+        }
+
+        private static RegistryKey GetAccelaBaseKey()
+        {
+            RegistryKey hklmReg;
+            RegistryKey accelaBaseKey;
+            try
+            {
+                hklmReg = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                Debug.WriteLine(hklmReg.ToString());
+                try
+                {
+                    accelaBaseKey = hklmReg.OpenSubKey(@"\Software\Accela Inc.");
+                    Debug.WriteLine(accelaBaseKey.GetSubKeyNames().ToString());
+                    return accelaBaseKey;
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show("Failed to open Accela key. Error: " + e.Message + " " + e.StackTrace);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Failed to open HKLM. Error: " + e.Message + " " + e.StackTrace);
+            }
+
+            return null;
+        }
+
+        public static List<string> GetAAVersions()
+        {
+            if(GetAccelaBaseKey() == null)
+            {
+                StringBuilder err = new StringBuilder();
+                err.AppendLine("Couldn't open Accela Base Key");
+                logger.LogError(err.ToString());
+                return null;
+            }
+            return new List<string>(GetAccelaBaseKey().OpenSubKey(@"\AA Base Installer\").GetSubKeyNames());
+        }
+
+        public static List<string> GetAAInstancesByVersion(string version)
+        {
+            string[] instancekeys = GetAccelaBaseKey().OpenSubKey(@"\AA Base Installer\" + version).GetSubKeyNames();
+            return new List<string>(instancekeys);
         }
     }
 }
---
+++ AA-ChangeDBConfig/Business/Logger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA_ChangeDBConfig.Business
+{
+    public class Logger
+    {
+        protected string logFile { get; set; }
+        public Logger(string _logFile)
+        {
+            logFile = _logFile;
+        }
+
+        public void LogInfo(string s)
+        {
+            ProcessWrite("- INFO: " + s);
+        }
+        public void LogWarn(string s)
+        {
+            ProcessWrite("- WARN: " + s);
+        }
+        public void LogError(string s)
+        {
+            ProcessWrite("- ERROR: " + s);
+        }
+        public void LogDebug(string s)
+        {
+            ProcessWrite("- DEBUG: " + s);
+        }
+        public void LogTrace(string s)
+        {
+            ProcessWrite("- TRACE: " + s);
+        }
+
+        Task ProcessWrite(string text)
+        {
+            return WriteTextAsync(logFile, text);
+        }
+
+        async Task WriteTextAsync(string filePath, string text)
+        {
+            text = DateTime.Now.ToString() + text;
+            byte[] encodedText = Encoding.Unicode.GetBytes(text);            
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Append, FileAccess.Write, FileShare.None,
+                bufferSize: 4096, useAsync: true))
+            {
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+            };
+        }
+    }
+}
