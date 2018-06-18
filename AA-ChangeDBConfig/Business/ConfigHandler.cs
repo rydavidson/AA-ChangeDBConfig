eae95dc8db84a433c6cedc4de43c435c195d2a14
Ryan Davidson <rydavidson@accela.com>
2018-06-18 08:58:46 -0600

    Added handling for non existent config items
    Added a check to ConfigHandler to make sure the config item is found and if not, don't try to update it

--- AA-ChangeDBConfig/Business/ConfigHandler.cs
+++ AA-ChangeDBConfig/Business/ConfigHandler.cs
@@ -41,16 +41,26 @@ namespace AA_ChangeDBConfig.Business
 
         public void WriteMSSQLConfigToFile(MSSQLConfig mssql)
         {
+
+            logger.LogToUI("Backing up " + PathToConfigFile);
+
+            if (File.Exists(PathToConfigFile + ".backup"))
+                File.Delete(PathToConfigFile + ".backup");
+
+            File.Copy(PathToConfigFile, PathToConfigFile + ".backup");
+
             WriteValueToConfig("av.db.host", mssql.serverHostname);
-            WriteValueToConfig("jetspeed.db.host", mssql.serverHostname);
+            WriteValueToConfig("av.jetspeed.db.host", mssql.serverHostname);
 
             WriteValueToConfig("av.db.sid", mssql.avDatabaseName);
             WriteValueToConfig("av.db.username", mssql.avDatabaseUser);
             WriteValueToConfig("av.db.password", mssql.GetAVDatabasePassword());
 
-            WriteValueToConfig("av.jetspeed.db.sid", mssql.avDatabaseName);
-            WriteValueToConfig("av.jetspeed.db.username", mssql.avDatabaseUser);
-            WriteValueToConfig("av.jetspeed.db.password", mssql.GetAVDatabasePassword());
+            WriteValueToConfig("av.jetspeed.db.sid", mssql.jetspeedDatabaseName);
+            WriteValueToConfig("av.jetspeed.db.username", mssql.jetspeedDatabaseUser);
+            WriteValueToConfig("av.jetspeed.db.password", mssql.GetJetspeedDatabasePassword());
+
+            logger.LogToUI("Updated config successfully");
 
         }
 
@@ -86,7 +96,7 @@ namespace AA_ChangeDBConfig.Business
             {
                 logger.LogError(string.Format("Directory not found, error: {0} {1}", dex.Message, dex.StackTrace));
             }
-            catch (SecurityException sex) 
+            catch (SecurityException sex)
             {
                 logger.LogError(string.Format("SecurityException getting config of {0}, error: {1} {2}", key, sex.Message, sex.StackTrace));
             }
@@ -124,27 +134,28 @@ namespace AA_ChangeDBConfig.Business
 
             string content = File.ReadAllText(PathToConfigFile, Encoding.Default); // read in the config file
             int indexOfKey = content.IndexOf(key); // get the start of the config item
-            int indexEndOfValue = content.IndexOf(Environment.NewLine, indexOfKey); // get the end of the config item
-            int indexOfEquals = content.IndexOf("=", indexOfKey); // get the index of the equals sign after the config item
-            string oldValue = content.Substring(indexOfEquals + 1, (indexEndOfValue - indexOfEquals)).Trim(); // get the old value
-            logger.LogTrace("Old value: " + oldValue);
+            if (indexOfKey < 0)
+                return; // exit if the config item isn't found
 
-            string oldConfigLine = content.Substring(indexOfKey, (indexEndOfValue - indexOfKey)); // get the entire line
-            string newConfigLine = oldConfigLine.Replace(oldValue, newValue); // replace the old value in the line with the new value
-            logger.LogToUI("New config line: " + newConfigLine);
+                int indexEndOfValue = content.IndexOf(Environment.NewLine, indexOfKey); // get the end of the config item
+                int indexOfEquals = content.IndexOf("=", indexOfKey); // get the index of the equals sign after the config item
+                string oldValue = content.Substring(indexOfEquals + 1, (indexEndOfValue - indexOfEquals)).Trim(); // get the old value
+                if (!oldValue.Contains("password"))
+                    logger.LogTrace("Old value: " + oldValue);
 
-            string newFile = content.Replace(oldConfigLine, newConfigLine);
+                string oldConfigLine = content.Substring(indexOfKey, (indexEndOfValue - indexOfKey)); // get the entire line
+                string newConfigLine = oldConfigLine.Replace(oldValue, newValue); // replace the old value in the line with the new value
+                if (!newConfigLine.Contains("password"))
+                    logger.LogToUI("New config line: " + newConfigLine);
 
-            logger.LogToUI("Backing up " + PathToConfigFile);
+                string newFile = content.Replace(oldConfigLine, newConfigLine);
 
-            if (File.Exists(PathToConfigFile + ".backup"))
-                File.Delete(PathToConfigFile + ".backup");
+                //logger.LogToUI("Writing new config");
 
-            File.Copy(PathToConfigFile, PathToConfigFile + ".backup");
-            logger.LogToUI("Writing new config");
+                File.WriteAllText(PathToConfigFile, newFile, Encoding.Default);
+                if (!key.Contains("password"))
+                    logger.LogTrace("Updated " + key + " to " + newConfigLine);
 
-            File.WriteAllText(PathToConfigFile, newFile, Encoding.Default);
-            logger.LogTrace("Updated " + key + " to " + newConfigLine);
         }
     }
 }
